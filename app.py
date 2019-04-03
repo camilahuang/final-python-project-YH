88797f140cae3405640827bf91bce027e45d3c23
Camilla <604937889@qq.com>
2019-04-03 14:13:49 +0100

    Index
    Creating an index.html

--- app.py
+++ app.py
@@ -1,30 +1,21 @@
 import os
-
 import requests
+import pyecharts
 
 from flask import Flask, render_template, request
+from pyecharts import GeoLines, Style
 
 app = Flask(__name__)
 
 @app.route("/")
 def say_hello():
-  return render_template("index.html") #render_template is used to redirect the linke to the html file instead of just typing words in "".
-
-#@app.route("/<name>")
-#def say_hello_to(name):
-#    return f"Hello {name}"
-
-@app.route("/<name>")
-def say_hello_to(name):
-  return render_template("index.html", user=name)
+  return render_template("index.html")
 
 @app.route("/feedback", methods=["POST"])
 def get_feedback():
-  # request.values is a dictionary holding any
-  # POST request data that's not already part of the URL
   data = request.values
-
   return render_template("feedback.html", form_data=data)
+
 
 if 'PORT' in os.environ:
      app.run(host='0.0.0.0', port=int(os.environ['PORT']))
