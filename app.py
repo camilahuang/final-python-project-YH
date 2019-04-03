ae1efe0008c709ebb66202c57b761603a6892bf5
Camilla <604937889@qq.com>
2019-04-03 15:10:35 +0100

    Web 2.0
    Link index and create contact sites

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 import os
 import requests
-import pyecharts
 
 from flask import Flask, render_template, request
-from pyecharts import GeoLines, Style
 
 app = Flask(__name__)
 
 @app.route("/")
+def homepage():
+  return render_template("index.html")
+
+@app.route("/index.html")
 def say_hello():
   return render_template("index.html")
 
-@app.route("/feedback", methods=["POST"])
+@app.route("/contact.html")
+def contactpage():
+  return render_template("contact.html")
+
+@app.route("/feedback.html", methods=["POST"])
 def get_feedback():
   data = request.values
   return render_template("feedback.html", form_data=data)
-
 
 if 'PORT' in os.environ:
      app.run(host='0.0.0.0', port=int(os.environ['PORT']))
